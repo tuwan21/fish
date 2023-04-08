f83de27a1ac7495dd620cd88c71c9817768dde0f
koya1 <2017431971@qq.com>
2023-04-08 17:07:03 +0800

    第一次提交

---
+++ weixin/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="container">
+			{{hello}}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+	const hello = ref("hello world!")
+
+</script>
+
+<style>
+	
+</style>
